a864f22d53c52a79f42c3acaa77c7ba0487ecc4c
Charlotte Chen <charlottechen21@gmail.com>
2016-12-30 23:05:55 -0500

    Site loaded

---
+++ app.js
@@ -0,0 +1,28 @@
+// set variables for environment
+var path_pkg = require('path');
+var express = require('express');
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
+app.use(express.static(path_pkg.join(__dirname, '/public')));
+console.log("__dirname = %s", path_pkg.resolve(__dirname+'/public'));
+
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
