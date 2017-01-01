23ac8154c28ae64237063d756456dacc9ddda5bb
Charlotte Chen <charlottechen21@gmail.com>
2016-12-31 23:35:39 -0500

    color palette

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm install --save rxjs
+npm install --save zone.js@0.7.2
+npm install --save @angular/core
+npm install --save @angular/common
+npm install --save @angular/compiler
+npm install --save @angular/http
+npm install --save @angular/platform-browser
+npm install --save @angular/platform-browser-dynamic
+npm install --save @angular/router
